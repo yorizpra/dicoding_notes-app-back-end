e8c70055d0ae6d37ed1b0a592e6b62bbda75ddcf
yorizpra <yogarizkipratama@gmail.com>
2024-12-27 23:05:28 +0700

    fix bugs host value

--- src/server.js
+++ src/server.js
@@ -4,11 +4,10 @@ const routes = require('./routes');
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
-    host: 'localhost',
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
-        origin: ['http://notesapp-v1.dicodingacademy.com'],
-        additionalHeaders: ['Access-Control-Allow-Origin', 'Access-Control-Allow-Headers'],
+        origin: ['*'],
       },
     },
   });
