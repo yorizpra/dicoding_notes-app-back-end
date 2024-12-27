d151cf9912bb571856d5dd6ffae1772014238f35
yorizpra <yogarizkipratama@gmail.com>
2024-12-27 21:51:59 +0700

    initial commit

---
+++ src/server.js
@@ -0,0 +1,22 @@
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+    routes: {
+      cors: {
+        origin: ['http://notesapp-v1.dicodingacademy.com'],
+        additionalHeaders: ['Access-Control-Allow-Origin', 'Access-Control-Allow-Headers'],
+      },
+    },
+  });
+
+  server.route(routes);
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
